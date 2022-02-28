a693a0bb849045486c509d25e9ee222b479df0d6
hyejin1231 <phj981231@naver.com>
2022-02-28 23:11:04 +0900

    글 상세보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
@@ -29,6 +30,12 @@ public class BoardController {
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
 		return "board/saveForm";
+	}
+	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.getOne(id));
+		return "board/detail";
 	}
 	
 }
